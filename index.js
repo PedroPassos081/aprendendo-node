4db496bc554ab86c5f690dcd8375fb5283354fbc
PedroPassos081 <pedro.passos081@gmail.com>
2023-08-29 18:06:09 -0300

    async e tratando erros

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto))
-    })
+const textotext = 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura
+    [2]}))
+
+    return resultados;
+  }
+
+    extraiLinks(textotext);
+
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-pegaArquivo('./arquivos/texto.md');
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(extraiLinks(texto))
+    }   catch (erro) {
+        trataErro(erro)
+    }
+}
+ pegaArquivo('./arquivos/texto.md');
+
