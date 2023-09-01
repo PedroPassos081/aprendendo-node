fa59c229b3a460a405c1e11270b97411ce232f9f
Pedro Passos <pedro.passos081@gmail.com>
2023-09-01 13:20:50 -0300

    deixando o codigo mais legivel

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-import fs from 'fs';
-import chalk from 'chalk';
-
-const textotext = 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+import fs from 'fs'
+import chalk from 'chalk'
 
 function extraiLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map(captura => ({[captura[1]]: captura
-    [2]}))
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+  const capturas = [...texto.matchAll(regex)]
+  const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }))
 
-    return resultados;
-  }
+  return resultados
+}
 
-    extraiLinks(textotext);
+extraiLinks(textotext)
 
 function trataErro(erro) {
-    console.log(erro)
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+  console.log(erro)
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
-    try {
-        const encoding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(extraiLinks(texto))
-    }   catch (erro) {
-        trataErro(erro)
-    }
+  try {
+    const encoding = 'utf-8'
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(extraiLinks(texto))
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
- pegaArquivo('./arquivos/texto.md');
-
+pegaArquivo('./arquivos/texto.md')
